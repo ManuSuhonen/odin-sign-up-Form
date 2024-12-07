f8b10145209c00dbbc0e6ca7806c6bd070ef0df0
Manu Suhonen <manu.suhonen@gmail.com>
2024-12-07 11:23:35 +0200

    added all input fields

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ console.clear();
 
 console.log("Helo World");
 
-window.getComputedStyle( $0, null )['fontFamily'];
+// window.getComputedStyle( $0, null )['fontFamily'];
 
-window.getComputedStyle( $0, null ).getPropertyValue( 'font-family' );
+// window.getComputedStyle( $0, null ).getPropertyValue( 'font-family' );
 
 // queryselectAll "For web developers (non-normative)" examples
-$$('.domintro')
 
-console.table($0,['index','value'])
+// $$('.domintro')
+
+// console.table($0,['index','value'])
