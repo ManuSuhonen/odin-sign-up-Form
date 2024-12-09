36989697fcec70d0ff8136ab688b97f41172a9e8
Manu Suhonen <manu.suhonen@gmail.com>
2024-12-09 18:42:01 +0200

    Finalization

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-console.clear();
-
-console.log("Helo World");
-
 myForm = document.forms['myform'];
-
-myForm ? null : console.error("myForm is", myForm);
 
 pwInput1 = myForm['password'];
 pwInput2 = myForm['confirm-password'];
 
-pwInput1 ? null : console.error("pwInput1 is", pwInput1);
-pwInput2 ? null : console.error("pwInput2 is", pwInput2);
-
 pwInput2.oninput = (obj) => {
     pwInput2.setCustomValidity(pwInput1.value != pwInput2.value ? 'Passwords do not match.' : '');
 }
+
+rightTop = document.querySelector('#right_side_1');
+logo = document.querySelector('#logo');
+logoHeight = window.getComputedStyle(logo).height
+
+window.addEventListener('resize', function () {
+    let topOffset = rightTop.offsetHeight - logo.offsetHeight;
+    logo.style.marginTop = topOffset + "px";
+});
